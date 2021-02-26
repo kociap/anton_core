46ae8b03546514f28881d2b059f8392a9c17e993
Piotr Kocia <p.kocia@wp.pl>
2021-02-26 10:56:46 +0100

    deprecated forward, move and as_const added ANTON_AS_CONST

--- public/anton/detail/traits_utility.hpp
+++ public/anton/detail/traits_utility.hpp
@@ -4,30 +4,31 @@
 
 #define ANTON_MOV(x) (static_cast<anton::remove_reference<decltype(x)>&&>(x))
 #define ANTON_FWD(x) (static_cast<decltype(x)&&>(x))
+#define ANTON_AS_CONST(x) (static_cast<anton::add_const<decltype(x)>&>(x))
 
 namespace anton {
     template<typename T>
-    [[nodiscard]] constexpr T&& forward(remove_reference<T>& v) {
+    [[deprecated]] [[nodiscard]] constexpr T&& forward(remove_reference<T>& v) {
         return static_cast<T&&>(v);
     }
 
     template<typename T>
-    [[nodiscard]] constexpr T&& forward(remove_reference<T>&& v) {
+    [[deprecated]] [[nodiscard]] constexpr T&& forward(remove_reference<T>&& v) {
         // We don't really care about safety, so we just throw it out the window
         // static_assert(!is_lvalue_reference<T>, "Can not forward an rvalue as an lvalue.");
         return static_cast<T&&>(v);
     }
 
     template<typename T>
-    [[nodiscard]] constexpr remove_reference<T>&& move(T&& v) {
+    [[deprecated]] [[nodiscard]] constexpr remove_reference<T>&& move(T&& v) {
         return static_cast<remove_reference<T>&&>(v);
     }
 
     template<typename T>
-    [[nodiscard]] constexpr add_const<T>& as_const(T& v) {
+    [[deprecated]] [[nodiscard]] constexpr add_const<T>& as_const(T& v) {
         return v;
     }
 
     template<typename T>
-    void as_const(T const&&) = delete;
+    [[deprecated]] void as_const(T const&&) = delete;
 } // namespace anton

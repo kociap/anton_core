40ef79c5ac652d7099a8c9636fa5cffe31cc80a4
Piotr Kocia <p.kocia@wp.pl>
2021-06-16 11:48:23 +0200

    changed make_integer_sequence to use the __make_integer_sequence intrinsic to reduce compiletimes

--- public/anton/detail/utility.hpp
+++ public/anton/detail/utility.hpp
@@ -11,29 +11,14 @@ namespace anton {
     template<typename T, T... Integers>
     using integer_sequence = Integer_Sequence<T, Integers...>;
 
-    namespace detail {
-        template<typename T, T, T, typename>
-        struct Make_Integer_Sequence;
-
-        template<typename T, T N, T X, T... Integers>
-        struct Make_Integer_Sequence<T, N, X, Integer_Sequence<T, Integers...>> {
-            using type = typename Make_Integer_Sequence<T, N, X + 1, Integer_Sequence<T, Integers..., X>>::type;
-        };
-
-        template<typename T, T N, T... Integers>
-        struct Make_Integer_Sequence<T, N, N, Integer_Sequence<T, Integers...>> {
-            using type = Integer_Sequence<T, Integers...>;
-        };
-    } // namespace detail
-
     // Make_Integer_Sequence
     // Creates an Integer_Sequence which starts at 0 and ends at N - 1.
     //
     template<typename T, T N>
     struct Make_Integer_Sequence {
-        using type = typename detail::Make_Integer_Sequence<T, N, 0, Integer_Sequence<T>>::type;
+        using type = __make_integer_seq<Integer_Sequence, T, N>;
     };
 
     template<typename T, T N>
-    using make_integer_sequence = typename Make_Integer_Sequence<T, N>::type;
+    using make_integer_sequence = __make_integer_seq<Integer_Sequence, T, N>;
 } // namespace anton

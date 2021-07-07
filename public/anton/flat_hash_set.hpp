fa9b7fb0f728ef7faff4702ae04601f16e345027
Piotr Kocia <p.kocia@wp.pl>
2021-07-08 00:28:52 +0200

    added missing swap include

--- public/anton/flat_hash_set.hpp
+++ public/anton/flat_hash_set.hpp
@@ -5,6 +5,7 @@
 #include <anton/functors.hpp>
 #include <anton/math/math.hpp>
 #include <anton/memory.hpp>
+#include <anton/swap.hpp>
 #include <anton/tags.hpp>
 #include <anton/type_traits.hpp>
 #include <anton/types.hpp>

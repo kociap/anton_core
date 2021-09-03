0cdda8842032cd41bd4c5ffac75457c17e395cc9
Piotr Kocia <p.kocia@wp.pl>
2021-09-03 11:01:24 +0200

    added missing include, added missing files to cmake

--- public/anton/iterators.hpp
+++ public/anton/iterators.hpp
@@ -2,5 +2,6 @@
 
 #include <anton/iterators/base.hpp>
 #include <anton/iterators/move.hpp>
+#include <anton/iterators/range.hpp>
 #include <anton/iterators/reverse.hpp>
 #include <anton/iterators/zip.hpp>

75f4c8811aaa7200351eeaaf234365363834dd58
Piotr Kocia <peter.kocia@gmail.com>
2023-10-04 11:05:31 +0200

    add STDERR_Stream and rename Console_Output to STDOUT_Stream

--- private/console.cpp
+++ private/console.cpp
@@ -8,38 +8,68 @@
 #endif
 
 namespace anton {
-    Console_Output::~Console_Output() {}
-
-    Console_Output::operator bool() const {
+    STDOUT_Stream::operator bool() const {
         return true;
     }
 
-    void Console_Output::flush() {
+    void STDOUT_Stream::flush() {
         fflush(stdout);
     }
 
-    void Console_Output::write(void const* buffer, i64 count) {
+    void STDOUT_Stream::write(void const* buffer, i64 count) {
         fwrite(buffer, count, 1, stdout);
     }
 
-    void Console_Output::write(Slice<u8 const> const buffer) {
+    void STDOUT_Stream::write(Slice<u8 const> const buffer) {
         fwrite(buffer.data(), buffer.size(), 1, stdout);
     }
 
-    void Console_Output::write(String_View const buffer) {
+    void STDOUT_Stream::write(String_View const buffer) {
         fwrite(buffer.data(), buffer.size_bytes(), 1, stdout);
     }
 
-    void Console_Output::put(char32 c) {
+    void STDOUT_Stream::put(char32 c) {
         fputc(c, stdout);
     }
 
-    void Console_Output::seek(Seek_Dir dir, i64 offset) {
+    void STDOUT_Stream::seek(Seek_Dir dir, i64 offset) {
         fseek(stdout, (long)offset, (int)(dir));
     }
 
-    i64 Console_Output::tell() {
+    i64 STDOUT_Stream::tell() {
         return ftell(stdout);
+    }
+
+    STDERR_Stream::operator bool() const {
+        return true;
+    }
+
+    void STDERR_Stream::flush() {
+        fflush(stderr);
+    }
+
+    void STDERR_Stream::write(void const* buffer, i64 count) {
+        fwrite(buffer, count, 1, stderr);
+    }
+
+    void STDERR_Stream::write(Slice<u8 const> const buffer) {
+        fwrite(buffer.data(), buffer.size(), 1, stderr);
+    }
+
+    void STDERR_Stream::write(String_View const buffer) {
+        fwrite(buffer.data(), buffer.size_bytes(), 1, stderr);
+    }
+
+    void STDERR_Stream::put(char32 c) {
+        fputc(c, stderr);
+    }
+
+    void STDERR_Stream::seek(Seek_Dir dir, i64 offset) {
+        fseek(stderr, (long)offset, (int)(dir));
+    }
+
+    i64 STDERR_Stream::tell() {
+        return ftell(stderr);
     }
 
     bool set_stdin_binary() {
--- public/anton/console.hpp
+++ public/anton/console.hpp
@@ -3,9 +3,24 @@
 #include <anton/stream.hpp>
 
 namespace anton {
-    struct Console_Output: public Output_Stream {
+    struct STDOUT_Stream: public Output_Stream {
     public:
-        virtual ~Console_Output() override;
+        virtual ~STDOUT_Stream() override = default;
+
+        [[nodiscard]] virtual operator bool() const override;
+
+        virtual void flush() override;
+        virtual void write(void const* buffer, i64 count) override;
+        virtual void write(Slice<u8 const> buffer) override;
+        virtual void write(String_View buffer) override;
+        virtual void put(char32) override;
+        virtual void seek(Seek_Dir dir, i64 offset) override;
+        virtual i64 tell() override;
+    };
+
+    struct STDERR_Stream: public Output_Stream {
+    public:
+        virtual ~STDERR_Stream() override = default;
 
         [[nodiscard]] virtual operator bool() const override;
 
